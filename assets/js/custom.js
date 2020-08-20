2065faa50bfcc2368941f5aeb7a23f6ceed631a5
LeoMalone <leomalone.18@gmail.com>
2020-08-20 19:55:38 -0400

    Major Refactor - v1.1

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -13,6 +13,25 @@ var myNameSpace = function() {
         }
     }
 
+    jQuery(function($) {
+        var $nav = $('#customNav');
+        var $win = $(window);
+        var winH = $win.height();   // Get the window height.
+    
+        $win.on("scroll", function () {
+            $nav.toggleClass("c_hide", $(this).scrollTop() < (winH - 1) );
+        }).on("resize", function(){ // If the user resizes the window
+           winH = $(this).height(); // you'll need the new height value
+        });
+    });
+    
+    $(window).on('load', function() {    
+        // Initial Logo/Buttons fade in
+        $("#photo").delay(500).fadeIn(2000);
+        $("#name").delay(500).fadeIn(2000);
+        $("#quote").delay(500).fadeIn(2000);
+     });
+
     return {
         scrollTo: scrollTo
     }
